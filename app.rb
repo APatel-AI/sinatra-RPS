93d8ee89ebe327d52356500f1fe33c41238ad77a
Arpan Patel <113603155+APatel-AI@users.noreply.github.com>
2024-02-17 11:35:40 +0000

    created header teemplate and play html pages

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:route_links)
+end
+
+get("/rock") do 
+
+  erb(:rock)
+
 end
